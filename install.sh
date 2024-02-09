e7ecc24dbaf0d4cf54ea03719fccdefea8e71518
Abel Ciccoli <abelcoli@outlook.fr>
2024-02-09 22:36:29 +0100

    feat(cna): installation is good

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 cp -r resources/ ~/go/bin/resources/ && cd src && go install
-=======
-mv -r resources/ ~/go/bin/resources/
-go install
->>>>>>> 72389f012d37a4a4d08d06cad4ff3c23eade78fa
